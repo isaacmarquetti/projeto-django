20922b3fdad2b80be9e125ec166fb88863eb626d
Isaac Marquetti <isaac.marquetti@gmail.com>
2022-06-02 22:06:37 -0300

    Começando o site

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home 1')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/home.html')
