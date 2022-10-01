a8323209e90974068ee43ff8a9c569f2747cc641
Isaac Marquetti <isaac.marquetti@gmail.com>
2022-10-01 17:43:57 -0300

    TDD

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -119,3 +119,7 @@ class RecipeViewsTest(RecipeTestBase):
 
         # Check if one recipe exists
         self.assertEqual(response.status_code, 404)
+
+    def teste_recipe_search_uses_correct_view_function(self):
+        view = resolve(reverse('recipes:search'))
+        self.assertIs(view.func, views.search)
